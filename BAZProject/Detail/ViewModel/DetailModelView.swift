8c0450dc6b1277c3f63b548e44dc731ce554f430
Alan Emiliano Ramirez Ayala <75821310+Replase@users.noreply.github.com>
2022-11-14 12:20:55 -0600

    Adding app to repository

---
+++ BAZProject/Detail/ViewModel/DetailModelView.swift
@@ -0,0 +1,14 @@
+//
+//  DetailModelView.swift
+//  BAZProject
+//
+//  Created by Alan Emiliano Ramirez Ayala on 25/10/22.
+//
+
+import Foundation
+
+final class DetailModelView {
+    
+    var dataArray: Movie?
+    
+}
